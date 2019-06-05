ff63f18fbd34aaa81c1c6b4326cbecb87c81bd00
jsbuddy <judecodes@yahoo.com>
2019-06-05 03:24:32 +0100

    fix(route): add /flag route

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import car from './routes/car';
 import auth from './routes/auth';
 import order from './routes/order';
+import flag from './routes/flag';
 import { authorize } from '../../middlewares/auth';
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.use('/auth', auth);
 router.use('/car', authorize, car);
 router.use('/order', authorize, order);
+router.use('/flag', authorize, flag);
 
 export default router;
