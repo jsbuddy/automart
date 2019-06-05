1202f6fce0c5910942930fba4a55e4489ba39965
jsbuddy <judecodes@yahoo.com>
2019-06-05 01:41:06 +0100

    feature(auth): create auth model using data structure

---
+++ src/models/auth.js
@@ -0,0 +1,52 @@
+import uuid from 'uuid';
+import { secret } from '../config/auth';
+import { comparePassword, generateToken, hashPassword } from '../helpers/auth';
+
+class Auth {
+  constructor() {
+    this.users = [];
+  }
+
+  async signup(user, callback) {
+    try {
+      const newUser = { ...user, id: uuid.v4(), createdAt: Date.now() };
+      const { id, firstName, lastName, email } = newUser;
+      const token = await generateToken({ id, firstName, lastName }, secret);
+      newUser.token = token;
+      newUser.password = await hashPassword(newUser.password);
+      this.users.push(newUser);
+      return callback(null, { id, firstName, lastName, email, token });
+    } catch (e) {
+      // TODO Handle Error
+      console.log('Signup error', e);
+      return callback({ status: 500, message: 'An error occurred' }, null);
+    }
+  }
+
+  async signin(user, callback) {
+    const { email, password } = user;
+    try {
+      const foundUser = this.findByEmail(email);
+      if (foundUser) {
+        const passwordsMatch = await comparePassword(password, foundUser.password);
+        if (passwordsMatch) {
+          const { id, firstName, lastName } = foundUser;
+          const token = await generateToken({ id, firstName, lastName }, secret);
+          return callback(null, { id, firstName, lastName, email, token });
+        }
+        return callback({ status: 400, message: 'Incorrect password' }, null);
+      }
+      return callback({ status: 404, message: 'User not found' }, null);
+    } catch (e) {
+      // TODO: Handle Error
+      console.log('Signin error', e);
+      return callback({ status: 500, message: 'An error occurred' }, null);
+    }
+  }
+
+  findByEmail(email) {
+    return this.users.find(user => user.email === email);
+  }
+}
+
+export default new Auth();
