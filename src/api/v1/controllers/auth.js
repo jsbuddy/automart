65567d3069dc60006b761a3158eff58517751b9e
jsbuddy <judecodes@yahoo.com>
2019-06-06 12:05:08 +0100

    refactor(auth): extract auth controller handlers to a new function

--- src/api/v1/controllers/auth.js
+++ src/api/v1/controllers/auth.js
@@ -1,18 +1,13 @@
 import AuthModel from '../../../models/auth';
 
+function handle(err, user, res, status) {
+  if (err) return res.status(err.status).json({ success: false, message: err.message });
+  return res.status(status).json({ success: true, user });
+}
+
 const User = {
-  signup: (req, res) => {
-    AuthModel.signup(req.body, (err, user) => {
-      if (err) return res.status(err.status).json({ success: false, message: err.message });
-      return res.status(201).json({ success: true, user });
-    });
-  },
-  signin: (req, res) => {
-    AuthModel.signin(req.body, (err, user) => {
-      if (err) return res.status(err.status).json({ success: false, message: err.message });
-      return res.status(200).json({ success: true, user });
-    });
-  },
+  signup: (req, res) => AuthModel.signup(req.body, (err, user) => handle(err, user, res, 201)),
+  signin: (req, res) => AuthModel.signin(req.body, (err, user) => handle(err, user, res, 200)),
 };
 
 export default User;
