d3958c9008d51d0f36143b987ee2bfc27b1a3907
jsbuddy <judecodes@yahoo.com>
2019-06-07 17:56:49 +0100

    chore(docs): update swagger.js

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './openapi.json';
+import swaggerDocument from '../../openapi.json';
 
 const router = Router();
 
