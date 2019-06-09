d20de4d6fbfccda9bf738c1ffe2f67e1dfbd2fc9
jsbuddy <judecodes@yahoo.com>
2019-06-09 02:18:34 +0100

    bug(authorization): fix jwt unhandled promise rejection error in authorize middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,11 +3,15 @@ import { verifyToken } from '../helpers/auth';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function authorize(req, res, next) {
-  if (!req.headers.authorization) return res.status(403).json({ success: false, message: 'No authorization token provided' });
-  const token = req.headers.authorization.split(' ')[1];
-  if (!token) return res.status(403).json({ success: false, message: 'No authorization token provided' });
-  const user = await verifyToken(token, secret);
-  if (!user) return res.status(403).json({ success: false, message: 'Invalid token' });
-  req.user = user;
-  return next();
+  try {
+    if (!req.headers.authorization) return res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) return res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
+    const user = await verifyToken(token, secret);
+    if (!user) return res.status(403).json({ success: false, message: 'Invalid token' });
+    req.user = user;
+    return next();
+  } catch (error) {
+    return res.status(403).json({ success: false, message: 'Invalid token' });
+  }
 }
