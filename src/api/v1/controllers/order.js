53a1225cfc97806cd8fa3fc867da1da7551e5ada
jsbuddy <judecodes@yahoo.com>
2019-06-09 03:11:01 +0100

    feature(order): check if car exists before creating new purchase order

--- src/api/v1/controllers/order.js
+++ src/api/v1/controllers/order.js
@@ -1,24 +1,27 @@
 import OrderModel from '../../../models/order';
+import CarModel from '../../../models/car';
 import { handleCreate } from '../../../helpers/callback';
+import { notfound, unauthorized } from '../../../helpers/response';
 
 const Order = {
-  create: (req, res) => handleCreate(OrderModel, { ...req.body, buyer: req.user.id }, res, 'order'),
+  create: (req, res) => {
+    const car = CarModel.findOne(req.body.carId);
+    if (!car) return notfound(res, 'There is no car with that id');
+    handleCreate(OrderModel, { ...req.body, buyer: req.user.id }, res, 'order');
+  },
   update: (req, res) => {
     const { id } = req.params;
     let data = req.body;
     let order = OrderModel.findOne(id);
-    if (order.status === 'pending') {
-      if (data.price) {
-        const { newPriceOffered: oldPrice, priceOffered } = order;
-        const oldPriceOffered = oldPrice || priceOffered;
-        const { price, priceOffered: newPriceOffered, ...newData } = data;
-        data = { ...newData, oldPriceOffered, newPriceOffered };
-      }
-      order = OrderModel.update(id, data);
-      res.status(200).json({ success: true, order });
-    } else {
-      res.status(401).json({ success: false });
+    if (order.status !== 'pending') return unauthorized(res);
+    if (data.price) {
+      const { newPriceOffered: oldPrice, priceOffered } = order;
+      const oldPriceOffered = oldPrice || priceOffered;
+      const { price, priceOffered: newPriceOffered, ...newData } = data;
+      data = { ...newData, oldPriceOffered, newPriceOffered };
     }
+    order = OrderModel.update(id, data);
+    res.status(200).json({ success: true, order });
   },
 };
 
