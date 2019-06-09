b12efcdf9312aa9947cc107761df527c2643a8d9
jsbuddy <judecodes@yahoo.com>
2019-06-10 00:10:11 +0100

    chore(auth): improve auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,19 @@
 import { secret } from '../config/auth';
 import { verifyToken } from '../helpers/auth';
+import { unauthorized } from '../helpers/response';
+import AuthModel from '../models/auth';
 
 export async function authorize(req, res, next) {
   try {
-    if (!req.headers.authorization) return res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
+    if (!req.headers.authorization) return unauthorized(res, 'No authorization token provided, login to get an authorization token');
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) return res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
-    const user = await verifyToken(token, secret);
-    if (!user) return res.status(403).json({ success: false, message: 'Invalid token' });
+    if (!token) return unauthorized(res, 'No authorization token provided, login to get an authorization token');
+    const payload = await verifyToken(token, secret);
+    const user = AuthModel.findById(payload.id);
+    if (!user) return unauthorized('Invalid Token');
     req.user = user;
     next();
   } catch (error) {
-    res.status(403).json({ success: false, message: 'Invalid token' });
+    unauthorized(res, 'Invalid token');
   }
 }
