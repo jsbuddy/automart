2e9355cf7efe7dd412dc74fb29dc9a90ac504a32
jsbuddy <judecodes@yahoo.com>
2019-06-09 02:40:48 +0100

    refactor(codeclimate): attempt to reduce code complexity and duplication

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,16 @@
 import { secret } from '../config/auth';
 import { verifyToken } from '../helpers/auth';
 
-// eslint-disable-next-line import/prefer-default-export
+// eslint-disable-next-line import/prefer-default-export, consistent-return
 export async function authorize(req, res, next) {
   try {
-    if (!req.headers.authorization) res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
+    if (!req.headers.authorization) return res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) {
-      res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
-    } else {
-      const user = await verifyToken(token, secret);
-      if (!user) res.status(403).json({ success: false, message: 'Invalid token' });
-      req.user = user;
-      next();
-    }
+    if (!token) return res.status(403).json({ success: false, message: 'No authorization token provided, login to get an authorization token' });
+    const user = await verifyToken(token, secret);
+    if (!user) return res.status(403).json({ success: false, message: 'Invalid token' });
+    req.user = user;
+    next();
   } catch (error) {
     res.status(403).json({ success: false, message: 'Invalid token' });
   }
