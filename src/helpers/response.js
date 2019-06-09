8cb2d1ed6c601df6c2be1623c9c75d620afffa5d
jsbuddy <judecodes@yahoo.com>
2019-06-09 03:14:49 +0100

    chore(repsonse): add notfound reponse

--- src/helpers/response.js
+++ src/helpers/response.js
@@ -1,5 +1,8 @@
 // This function was created based on a refactoring to remove duplicate codes found by codeclimate
-// eslint-disable-next-line import/prefer-default-export
 export function unauthorized(res, message = 'Unauthorized') {
   return res.status(401).json({ success: false, message });
+}
+
+export function notfound(res, message = 'Not found') {
+  return res.status(404).json({ success: false, message });
 }
