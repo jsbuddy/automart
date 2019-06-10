4119a9e763f20707b3247b2e65ca1901a5532944
jsbuddy <judecodes@yahoo.com>
2019-06-10 01:04:12 +0100

    refactor(response): fix code duplication issue

--- src/helpers/response.js
+++ src/helpers/response.js
@@ -1,8 +1,13 @@
 // This function was created based on a refactoring to remove duplicate codes found by codeclimate
+
+function send(res, code, data) {
+  return res.status(code).json(data);
+}
+
 export function unauthorized(res, message = 'Unauthorized') {
-  return res.status(401).json({ success: false, message });
+  return send(res, 401, { success: false, message });
 }
 
 export function notfound(res, message = 'Not found') {
-  return res.status(404).json({ success: false, message });
+  return send(res, 404, { success: false, message });
 }
