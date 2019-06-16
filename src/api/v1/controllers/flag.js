996dcd364bc36f53bc1d8f761142b08f546f318e
jsbuddy <judecodes@yahoo.com>
2019-06-16 23:22:32 +0100

    feature(route): create delete flag controller

--- src/api/v1/controllers/flag.js
+++ src/api/v1/controllers/flag.js
@@ -1,12 +1,18 @@
 import FlagModel from '../../../models/flag';
 import { handleCreate } from '../../../helpers/callback';
-import { success } from '../../../helpers/response';
+import { success, unauthorized } from '../../../helpers/response';
 
 const Flag = {
   create: (req, res) => handleCreate(FlagModel, { ...req.body, creator: req.user.id }, res, 'flag'),
   getAll: async (req, res) => {
     const flags = await FlagModel.findAll();
     return success(res, null, { flags });
+  },
+  delete: async (req, res) => {
+    if (!req.user.isAdmin) return unauthorized(res);
+    const { id } = req.params;
+    await FlagModel.delete(id);
+    return success(res, 'Flag deleted successfully', { success: true });
   },
 };
 
