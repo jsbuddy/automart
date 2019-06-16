38967342baf6590e0288782f609dbc5d5b6ca4ed
jsbuddy <judecodes@yahoo.com>
2019-06-16 23:32:02 +0100

    chore(database): use remote database connection string

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,8 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-// const connectionString = 'postgres://uvfgvozzyuqjxe:0ac78705e75f3256c1cd0431e54e30ce2e7234f21e5595e12175c3b207154124@ec2-75-101-147-226.compute-1.amazonaws.com:5432/d4djeuf1hf9gpl?ssl=true';
-const pool = new Pool();
+const connectionString = 'postgres://uvfgvozzyuqjxe:0ac78705e75f3256c1cd0431e54e30ce2e7234f21e5595e12175c3b207154124@ec2-75-101-147-226.compute-1.amazonaws.com:5432/d4djeuf1hf9gpl?ssl=true';
+const pool = new Pool({ connectionString });
 
 (async () => {
   const client = await pool.connect();
