f751bea7c662303987e7df2d3b00d01013b1c094
jsbuddy <judecodes@yahoo.com>
2019-06-16 23:20:52 +0100

    feature(route): create deleteUser controller

--- src/api/v1/controllers/auth.js
+++ src/api/v1/controllers/auth.js
@@ -1,4 +1,5 @@
 import AuthModel from '../../../models/auth';
+import { success, unauthorized } from '../../../helpers/response';
 
 function handle(err, data, res, status) {
   if (err) return res.status(err.status).json({ success: false, message: err.message });
@@ -8,6 +9,12 @@ function handle(err, data, res, status) {
 const User = {
   signup: (req, res) => AuthModel.signup(req.body, (err, data) => handle(err, data, res, 201)),
   signin: (req, res) => AuthModel.signin(req.body, (err, data) => handle(err, data, res, 200)),
+  deleteUser: async (req, res) => {
+    if (!req.user.isAdmin) return unauthorized(res);
+    const { id } = req.params;
+    await AuthModel.deleteUser(id);
+    return success(res, 'User Deleted Successfully', { success: true });
+  },
 };
 
 export default User;
