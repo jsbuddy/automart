08065f5087ca9a4a6a2c55c70ba55e4c8ce0275e
jsbuddy <judecodes@yahoo.com>
2019-06-16 23:24:47 +0100

    feature(api): create delete user route

--- src/api/v1/routes/auth.js
+++ src/api/v1/routes/auth.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import expressJoi from 'express-joi-validator';
 import AuthController from '../controllers/auth';
 import { userSigninSchema, userSignupSchema } from '../../../lib/schemas';
+import { authorize } from '../../../middlewares/auth';
 
 const router = Router();
 
 router.post('/signup', expressJoi(userSignupSchema), AuthController.signup);
 router.post('/signin', expressJoi(userSigninSchema), AuthController.signin);
+router.delete('/user/:id', authorize, AuthController.deleteUser);
 
 export default router;
