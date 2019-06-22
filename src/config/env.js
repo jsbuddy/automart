043ee0ae9e4c902fc52dd3a9ae8a110d797701ae
jsbuddy <judecodes@yahoo.com>
2019-06-22 06:02:49 +0100

    chore(env): move jwt secret to env file

--- src/config/env.js
+++ src/config/env.js
@@ -7,6 +7,7 @@ const config = {
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  SECRET: process.env.SECRET,
 };
 
 export default config;
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,4 @@
-import { secret } from '../config/auth';
+import env from '../config/env';
 import { verifyToken } from '../helpers/auth';
 import { unauthorized } from '../helpers/response';
 import AuthModel from '../models/auth';
@@ -8,7 +8,7 @@ export async function authorize(req, res, next) {
     if (!req.headers.authorization) return unauthorized(res, 'No authorization token provided, login to get an authorization token');
     const token = req.headers.authorization.split(' ')[1];
     if (!token) return unauthorized(res, 'No authorization token provided, login to get an authorization token');
-    const payload = await verifyToken(token, secret);
+    const payload = await verifyToken(token, env.SECRET);
     const user = await AuthModel.findById(payload.id);
     if (!user) return unauthorized('Invalid Token');
     req.user = user;
