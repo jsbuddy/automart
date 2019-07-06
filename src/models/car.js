7e0fd560b482b599de0ddc4ad2a349913fbab7a9
jsbuddy <judecodes@yahoo.com>
2019-07-06 16:09:40 +0100

    chore(sql): improve sql queries for findOne, findAll and findAllByUser

--- src/models/car.js
+++ src/models/car.js
@@ -5,21 +5,23 @@ class Car extends Base {
   static async findOne(id) {
     return (await db.query(`
       SELECT cars.*, row_to_json(row(users."id", users."firstName", users."lastName", users."email", users."address")::TUser) as "owner" 
-      FROM ${this.model()} INNER JOIN users ON cars.id = $1;
+      FROM ${this.model()} JOIN users ON cars.owner = users.id
+      WHERE cars.id = $1;
       `, [id])).rows[0];
   }
 
   static async findAll() {
     return (await db.query(`
       SELECT cars.*, row_to_json(row(users."id", users."firstName", users."lastName", users."email", users."address")::TUser) as "owner" 
-      FROM ${this.model()} INNER JOIN users ON cars.owner = users.id;
+      FROM ${this.model()} JOIN users ON cars.owner = users.id;
     `)).rows;
   }
 
   static async findAllByUser(owner) {
     return (await db.query(`
       SELECT cars.*, row_to_json(row(users."id", users."firstName", users."lastName", users."email", users."address")::TUser) as "owner" 
-      FROM ${this.model()} INNER JOIN users ON cars.owner = $1;
+      FROM ${this.model()} JOIN users ON users.id = cars.owner 
+      WHERE users.id = $1;
     `, [owner])).rows;
   }
 
