088b61ffb2c41031fcbe06a86ae6036f8ec65417
jsbuddy <judecodes@yahoo.com>
2019-07-07 11:33:41 +0100

    feature(ui): create theme file for theme switching

---
+++ ui/assets/js/theme.js
@@ -0,0 +1,8 @@
+(function setTheme() {
+  const doc = document.documentElement;
+  doc.className = localStorage.getItem('theme') || '';
+})();
+
+function themeBtnText(theme) {
+  return theme === 'dark' ? `<i class="fa fa-sun mr-2"></i>Light theme</a>` : `<i class="fa fa-moon mr-2"></i>Dark theme</a>`;
+}
