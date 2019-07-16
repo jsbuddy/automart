ede80a6f554b4203c9c59f086862f475e4304b73
jsbuddy <judecodes@yahoo.com>
2019-07-16 21:36:00 +0100

    chore(docs): remove swagger docs and all related files

--- src/docs/swagger.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../../openapi.json';
-
-const router = Router();
-
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-export default router;
